72a10cc2e2980b0dad0530407fbbc2092838b260
yashsehgal <yashsehgal.work@gmail.com>
2021-05-14 00:08:50 +0530

    Blog Cards added with CSS styles

---
+++ src/Components/BlogCard/BlogCard.js
@@ -0,0 +1,31 @@
+
+import "./BlogCard.css";
+
+const BlogCard = ({BlogCard_Details}) => {
+  console.log(BlogCard_Details);
+  return (
+    <div className="BlogCard">
+      <img src={BlogCard_Details["bg_image"]} 
+            alt="blog_demo_image" 
+            className="blog_demo_image"
+            />
+      <h3>
+        {BlogCard_Details["blog_title"]}
+      </h3>
+      <span>
+        <img src={BlogCard_Details["blog_author_profile_photo"]} 
+              alt="author-profile" 
+              className="author_profile_image"
+              />
+        <p>By, {BlogCard_Details["blog_author"]}</p>
+      </span>
+      <a href={BlogCard_Details["blog_url"]} target="_blank">
+        <button className="primaryButton">
+          Read Blog
+        </button>
+      </a>
+    </div>
+  )
+};
+
+export default BlogCard;
