105115203a59d9eefc57e960c79021bb3171a914
yashsehgal <yashsehgal.work@gmail.com>
2021-06-08 13:39:10 +0530

    new event details added

--- src/utils/events.js
+++ src/utils/events.js
@@ -2,6 +2,11 @@
 const EventDetails = {
   "Events": [
     {
+      "event_title": "Demystifying Data Science",
+      "event_description": "This event was based on understanding the basics of Data Science with the speaker Dr. Neha Sharma. She is working with Tata Consultancy Services and is also a Founder Secretary of Society for Data Science.",
+      "event_date": "12/06/2021"
+    },
+    {
       "event_title": "Induction Programme",
       "event_description": "Introduced MUACM to our student community and guided them about the upcoming sessions, events and roles.",
       "event_date": "31/03/2021"
