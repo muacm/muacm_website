f5820f9a71017a2e150dd69d10ee9e7eda9024ac
yashsehgal <yashsehgal.work@gmail.com>
2021-07-13 13:31:35 +0530

    web dev bootcamp event details added, with upcoming and done events feature

--- src/utils/events.js
+++ src/utils/events.js
@@ -1,24 +1,34 @@
 const EventDetails = {
   Events: [
     {
+      event_title: "Web development BootCamp",
+      event_description: "Medicaps University ACM Student Chapter (MUACM) in collaboration with Google Developers Group, Indore is hosting a 10 day Web Development BootCamp! Creating a web app using HTML, CSS, React and how to use Git and GitHub will be taught from scratch.",
+      event_date: "July 22, 2021 - Aug 01, 2021",
+      status: "upcoming",
+      registration_url: "https://bit.ly/MUACM-WebDev-Bootcamp"
+    },
+    {
       event_title: "Demystifying Data Science",
       event_description:
         "This event was based on understanding the basics of Data Science with the speaker Dr. Neha Sharma. She is working with Tata Consultancy Services and is also a Founder Secretary of Society for Data Science.",
       event_date: "12/06/2021",
+      status: "done"
     },
     {
       event_title: "Induction Programme",
       event_description:
         "Introduced MUACM to our student community and guided them about the upcoming sessions, events and roles.",
       event_date: "31/03/2021",
+      status: "done"
     },
     {
       event_title: "Community Connect - Introduction to Open Source",
       event_description:
         "This session was an introduction to Open Source for Beginners. Yash Sehgal discussed guided folks on how to get started with Open Source contribution.",
       event_date: "08/05/2021",
-    },
-  ],
+      status: "done"
+    }
+  ]
 };
 
 export default EventDetails;
